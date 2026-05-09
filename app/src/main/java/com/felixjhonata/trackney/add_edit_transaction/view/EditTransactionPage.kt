f991768844091a2216ee0e7d80f90444395585ed
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2026-05-09 23:03:05 +0700

    Integrate ViewModel and navigation logic into EditTransactionPage
    * Inject `AddEditTransactionViewModel` using `hiltViewModel`.
    * Observe the UI state and collect UI events using `LaunchedEffect` and `collectAsState`.
    * Implement back navigation logic when the `NavigateBack` event is received.
    * Update `AddEditTransactionPageContent` to use the dynamic UI state and the ViewModel's event handler callback.

--- app/src/main/java/com/felixjhonata/trackney/add_edit_transaction/view/EditTransactionPage.kt
+++ app/src/main/java/com/felixjhonata/trackney/add_edit_transaction/view/EditTransactionPage.kt
@@ -1,21 +1,38 @@
 package com.felixjhonata.trackney.add_edit_transaction.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
-import com.felixjhonata.trackney.add_edit_transaction.model.AddEditTransactionUiState
+import com.felixjhonata.trackney.add_edit_transaction.model.AddEditTransactionUiEvent
 import com.felixjhonata.trackney.add_edit_transaction.model.ModifyTransactionType
+import com.felixjhonata.trackney.add_edit_transaction.viewmodel.AddEditTransactionViewModel
 
 @Composable
 fun EditTransactionPage(
     navBackStack: NavBackStack<NavKey>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: AddEditTransactionViewModel = hiltViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    LaunchedEffect(Unit) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+               AddEditTransactionUiEvent.NavigateBack -> {
+                   navBackStack.removeLastOrNull()
+               }
+            }
+        }
+    }
+
     AddEditTransactionPageContent(
         ModifyTransactionType.EDIT,
-        AddEditTransactionUiState(),
-        {},
+        uiState,
+        viewModel::onUserEvent,
         modifier = modifier
     )
 }
