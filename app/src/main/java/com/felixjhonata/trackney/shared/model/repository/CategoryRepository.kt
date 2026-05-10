dcbedc4cc982e39b90f8da66d574e02e344a6b30
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2026-05-10 16:38:17 +0700

    Configure Singleton scope for database and repository components
    * Add the `@Singleton` annotation to the database, `CategoryDao`, and `TransactionDao` providers in `DatabaseModule`.
    * Mark `CategoryRepository` and `TransactionRepository` as singletons to ensure a single instance is shared across the application.

--- app/src/main/java/com/felixjhonata/trackney/shared/model/repository/CategoryRepository.kt
+++ app/src/main/java/com/felixjhonata/trackney/shared/model/repository/CategoryRepository.kt
@@ -2,7 +2,9 @@ package com.felixjhonata.trackney.shared.model.repository
 
 import com.felixjhonata.trackney.shared.model.dao.CategoryDao
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class CategoryRepository @Inject constructor(
     private val categoryDao: CategoryDao
 ) {
--- app/src/main/java/com/felixjhonata/trackney/shared/model/repository/TransactionRepository.kt
+++ app/src/main/java/com/felixjhonata/trackney/shared/model/repository/TransactionRepository.kt
@@ -3,7 +3,9 @@ package com.felixjhonata.trackney.shared.model.repository
 import com.felixjhonata.trackney.shared.model.dao.TransactionDao
 import com.felixjhonata.trackney.shared.model.entity.Transaction
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class TransactionRepository @Inject constructor(
     private val transactionDao: TransactionDao
 ) {
