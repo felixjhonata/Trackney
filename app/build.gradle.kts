2119d2c86689b0ee5d03699e436aa9fbb2b4a6eb
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2026-06-11 20:40:57 +0700

    added kover dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.kotlinx.kover)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
 }
